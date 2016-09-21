70440dd1efc84e70b44d0701af9d1f86036de37a
krishna-thayakotli <thayakoti.krishna@gmail.com>
2016-09-21 19:23:00 +0530

    added Map Example

---
+++ HelloWorld/WEB-INF/src/com/insta/hms/MapExample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.insta.hms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author krishna
+ *
+ */
+public class MapExample {
+	
+	public static void main(String[] args) {
+		Map m = new HashMap();
+	}
+
+}

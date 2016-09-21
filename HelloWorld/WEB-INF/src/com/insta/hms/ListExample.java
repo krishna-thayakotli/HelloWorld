e7e5506e2c00afdee723f4f6bc8f9dd556c262fa
krishna-thayakotli <thayakoti.krishna@gmail.com>
2016-09-21 17:22:25 +0530

    printing list size

--- HelloWorld/WEB-INF/src/com/insta/hms/ListExample.java
+++ HelloWorld/WEB-INF/src/com/insta/hms/ListExample.java
@@ -3,7 +3,8 @@
  */
 package com.insta.hms;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author krishna
@@ -12,7 +13,9 @@ import java.util.Collections;
 public class ListExample {
 	
 	public static void main(String[] args) {
-		System.out.println(Collections.EMPTY_LIST);
+		List<String> l = new ArrayList<String>();
+		
+		System.out.println(l.size());
 	}
 
 }

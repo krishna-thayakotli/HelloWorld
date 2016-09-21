7a233fa02ca1923378cd46e8acc6064d5a951e69
krishna-thayakotli <thayakoti.krishna@gmail.com>
2016-09-21 15:51:45 +0530

    Random Number added

---
+++ HelloWorld/WEB-INF/src/com/insta/hms/RandomNum.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.insta.hms;
+
+/**
+ * @author krishna
+ *
+ */
+public class RandomNum {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+	}
+
+}

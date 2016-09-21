3d9754893ac0f5a45f21a8eb677dda42d55dcce7
krishna-thayakotli <thayakoti.krishna@gmail.com>
2016-09-21 17:15:24 +0530

    added List Example

---
+++ HelloWorld/WEB-INF/src/com/insta/hms/ListExample.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.insta.hms;
+
+import java.util.Collections;
+
+/**
+ * @author krishna
+ *
+ */
+public class ListExample {
+	
+	public static void main(String[] args) {
+		System.out.println(Collections.EMPTY_LIST);
+	}
+
+}
